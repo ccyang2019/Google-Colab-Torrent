597e39972d05738b250ec3f1c0b38bc76be6e649
ccyang2019 <47751514+ccyang2019@users.noreply.github.com>
2020-09-03 13:46:17 +0800

    Update my_lib.py

--- res/my_lib.py
+++ res/my_lib.py
@@ -18,3 +18,35 @@ def findProcess(process, command="", isPid=False):
                     pass
             except:
                 continue
+                
+                
+#
+def runSh(args, *, output=False, shell=False, cd=None):
+    import subprocess, shlex
+
+    if not shell:
+        if output:
+            proc = subprocess.Popen(
+                shlex.split(args), stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=cd
+            )
+            while True:
+                output = proc.stdout.readline()
+                if output == b"" and proc.poll() is not None:
+                    return
+                if output:
+                    print(output.decode("utf-8").strip())
+        return subprocess.run(shlex.split(args), cwd=cd).returncode
+    else:
+        if output:
+            return (
+                subprocess.run(
+                    args,
+                    shell=True,
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.STDOUT,
+                    cwd=cd,
+                )
+                .stdout.decode("utf-8")
+                .strip()
+            )
+        return subprocess.run(args, shell=True, cwd=cd).returncode
